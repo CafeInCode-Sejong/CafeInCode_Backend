6630f8d40068309238638564b2e34d22a83d1f7d
fnsl1026 <fnsl1026@naver.com>
2024-10-17 19:33:53 +0900

    initial commit

---
+++ src/main/java/com/cafeincode/CafeincodeApplication.java
@@ -0,0 +1,13 @@
+package com.cafeincode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeincodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeincodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cafeincode/CafeincodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafeincode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeincodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
