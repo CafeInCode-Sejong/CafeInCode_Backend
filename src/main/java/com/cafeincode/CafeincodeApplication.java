66248745ce841455d616b1ae837efe5de1553eb5
rtaeho <ryou0920@naver.com>
2024-10-18 00:30:26 +0900

    CIC-5 연결테스트

--- src/main/java/com/cafeincode/CafeincodeApplication.java
+++ src/main/java/com/cafeincode/CafeincodeApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CafeincodeApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CafeincodeApplication.class, args);
 	}
